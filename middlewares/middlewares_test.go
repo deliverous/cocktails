f3cc65678bdca52ac8ad27cf2a54e48400ae20d5
Olivier Albiez <olivier.albiez@deliverous.com>
2015-03-10 22:29:09 +0100

    middleware unit test improved

--- middlewares/middlewares_test.go
+++ middlewares/middlewares_test.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -11,32 +12,52 @@ func Test_Chain_NoMiddleware(t *testing.T) {
 		"[APP]")
 }
 
-func Test_Chain_ThenNil(t *testing.T) {
+func Test_Chain_Then_WithNil_ShouldUseVoidHandler(t *testing.T) {
 	ensureRequestContains(t,
 		Chain(taggerMiddleware("[M1]")).Then(nil),
 		"[M1]")
 }
 
-func Test_Chain_Then(t *testing.T) {
+func Test_Chain_Then_WithHandler_ShouldCallTheHandler(t *testing.T) {
 	ensureRequestContains(t,
 		Chain(taggerMiddleware("[M1]")).Then(taggerHandler("[APP]")),
 		"[M1][APP]")
 }
 
-func Test_Chain_Concat(t *testing.T) {
-	first := Chain(taggerMiddleware("[M1]"))
-	second := Chain(taggerMiddleware("[M2]"), taggerMiddleware("[M3]"))
-	full := first.Concat(second)
-	ensureRequestContains(t, first.Then(taggerHandler("[APP]")), "[M1][APP]")
-	ensureRequestContains(t, second.Then(taggerHandler("[APP]")), "[M2][M3][APP]")
+func Test_Chain_Concat_ShouldConcatTheTwoChains(t *testing.T) {
+	full := Chain(taggerMiddleware("[M1]")).Concat(Chain(taggerMiddleware("[M2]"), taggerMiddleware("[M3]")))
 	ensureRequestContains(t, full.Then(taggerHandler("[APP]")), "[M1][M2][M3][APP]")
 }
 
-func Test_Chain_Extend(t *testing.T) {
+func Test_Chain_Concat_ShouldNotModifyTheReceiver(t *testing.T) {
+	first := Chain(taggerMiddleware("[M1]"))
+	first.Concat(Chain(taggerMiddleware("[M2]"), taggerMiddleware("[M3]")))
+	ensureRequestContains(t, first.Then(taggerHandler("[APP]")), "[M1][APP]")
+}
+
+func Test_Chain_Concat_ShouldNotModifyTheArgument(t *testing.T) {
+	second := Chain(taggerMiddleware("[M2]"), taggerMiddleware("[M3]"))
+	Chain(taggerMiddleware("[M1]")).Concat(second)
+	ensureRequestContains(t, second.Then(taggerHandler("[APP]")), "[M2][M3][APP]")
+}
+
+func Test_Chain_Extend_ShouldExtendsTheChainWithGivenMiddleware(t *testing.T) {
+	full := Chain(taggerMiddleware("[M1]")).Extend(taggerMiddleware("[M2]"), taggerMiddleware("[M3]"))
+	ensureRequestContains(t, full.Then(taggerHandler("[APP]")), "[M1][M2][M3][APP]")
+}
+
+func Test_Chain_Extend_ShouldNotModifyTheReceiver(t *testing.T) {
+	first := Chain(taggerMiddleware("[M1]"))
+	first.Extend(taggerMiddleware("[M2]"), taggerMiddleware("[M3]"))
+	ensureRequestContains(t, first.Then(taggerHandler("[APP]")), "[M1][APP]")
+}
+
+func Test_Chain_ExtendShouldBeImmutable(t *testing.T) {
 	first := Chain(taggerMiddleware("[M1]"))
 	full := first.Extend(taggerMiddleware("[M2]"), taggerMiddleware("[M3]"))
-	ensureRequestContains(t, first.Then(taggerHandler("[APP]")), "[M1][APP]")
-	ensureRequestContains(t, full.Then(taggerHandler("[APP]")), "[M1][M2][M3][APP]")
+	if repr(&first.builders[0]) == repr(&full.builders[0]) {
+		t.Errorf("Extand failed to be immutable")
+	}
 }
 
 func ensureRequestContains(t *testing.T, handler http.Handler, expectedTrace string) {
@@ -63,4 +84,8 @@ func taggerMiddleware(tag string) MiddlewareBuilder {
 			next.ServeHTTP(writer, request)
 		})
 	}
+}
+
+func repr(v interface{}) string {
+	return fmt.Sprintf("%#v", v)
 }
