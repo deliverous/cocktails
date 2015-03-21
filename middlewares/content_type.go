37e0f4fd279717aa8040c4670253be96184eddd9
Olivier Albiez <olivier.albiez@deliverous.com>
2015-03-21 18:11:13 +0100

    ContentType middleware added

---
+++ middlewares/content_type.go
@@ -0,0 +1,87 @@
+package middlewares
+
+import (
+	"mime"
+	"net/http"
+	"strings"
+)
+
+type ContentTypeChecker struct {
+	AcceptedContents []string
+	AcceptedCharsets []string
+	AssumedCharset   string
+	ErrorHandler     http.Handler
+}
+
+func NewContentTypeChecker() *ContentTypeChecker {
+	return &ContentTypeChecker{
+		AssumedCharset:   "UTF-8",
+		AcceptedCharsets: []string{"UTF-8"},
+		ErrorHandler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(http.StatusUnsupportedMediaType)
+		}),
+	}
+}
+
+func (m *ContentTypeChecker) SetAcceptedContents(contents ...string) *ContentTypeChecker {
+	m.AcceptedContents = contents
+	return m
+}
+
+func (m *ContentTypeChecker) SetAcceptedCharset(charsets ...string) *ContentTypeChecker {
+	m.AcceptedCharsets = make([]string, len(charsets))
+	for i, charset := range charsets {
+		m.AcceptedCharsets[i] = strings.ToUpper(charset)
+	}
+	return m
+}
+
+func (m *ContentTypeChecker) SetAssumedCharset(charset string) *ContentTypeChecker {
+	m.AssumedCharset = charset
+	return m
+}
+
+func (m *ContentTypeChecker) SetErrorHandler(handler http.Handler) *ContentTypeChecker {
+	m.ErrorHandler = handler
+	return m
+}
+
+func (m *ContentTypeChecker) acceptContent(value string) bool {
+	if len(m.AcceptedContents) == 0 {
+		return true
+	}
+	for _, content := range m.AcceptedContents {
+		if value == content {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *ContentTypeChecker) acceptCharset(value string) bool {
+	if len(m.AcceptedCharsets) == 0 {
+		return true
+	}
+	value = strings.ToUpper(value)
+	for _, charset := range m.AcceptedCharsets {
+		if value == charset {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *ContentTypeChecker) Check(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		mediatype, params, _ := mime.ParseMediaType(request.Header.Get("Content-Type"))
+		charset, ok := params["charset"]
+		if !ok {
+			charset = m.AssumedCharset
+		}
+		if m.acceptContent(mediatype) && m.acceptCharset(charset) {
+			next.ServeHTTP(writer, request)
+		} else {
+			m.ErrorHandler.ServeHTTP(writer, request)
+		}
+	})
+}
---
+++ middlewares/content_type_test.go
@@ -0,0 +1,91 @@
+package middlewares
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_ContentType_WithNoSpecifiedContentType_ShouldAcceptAnything(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().Check).Then(nil)
+	acceptContentType(t, handler, "application/json")
+	acceptContentType(t, handler, "text/hml")
+	acceptContentType(t, handler, "polop")
+}
+
+func Test_ContentType_WithSpecifiedContentType_ShouldAcceptThisContentType(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").Check).Then(nil)
+	acceptContentType(t, handler, "application/json")
+}
+
+func Test_ContentType_WithSpecifiedContentType_ShouldRejectOtherContentType(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").Check).Then(nil)
+	rejectContentType(t, handler, "text/html")
+}
+
+func Test_ContentType_WithMultipleSpecifiedContentType_ShouldAcceptAll(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json", "text/xml").Check).Then(nil)
+	acceptContentType(t, handler, "application/json")
+	acceptContentType(t, handler, "text/xml")
+	rejectContentType(t, handler, "text/html")
+}
+
+func Test_ContentType_WithSpecifiedContentTypeAndCharset_ShouldAcceptThisContentTypeWithTheCharset(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").SetAcceptedCharset("UTF-16").Check).Then(nil)
+	acceptContentType(t, handler, "application/json; charset=UTF-16")
+	rejectContentType(t, handler, "application/json; charset=UTF-8")
+}
+
+func Test_ContentType_CharsetIsCaseInsensitive(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").SetAcceptedCharset("UTF-16").Check).Then(nil)
+	acceptContentType(t, handler, "application/json; charset=utf-16")
+	acceptContentType(t, handler, "application/json; charset=uTf-16")
+	acceptContentType(t, handler, "application/json; charset=UTF-16")
+}
+
+func Test_ContentType_CanSpecifyTheAssumedCharsetWhenMissingInContentType(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").SetAcceptedCharset("UTF-8").SetAssumedCharset("UTF-16").Check).Then(nil)
+	rejectContentType(t, handler, "application/json")
+	acceptContentType(t, handler, "application/json; charset=UTF-8")
+}
+
+func Test_ContentType_AssumedCharsetIsUTF8ByDefault(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").SetAcceptedCharset("UTF-8").Check).Then(nil)
+	acceptContentType(t, handler, "application/json")
+}
+
+func Test_ContentType_DefaultCharsetIsUTF8(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").Check).Then(nil)
+	acceptContentType(t, handler, "application/json; charset=UTF-8")
+	rejectContentType(t, handler, "application/json; charset=UTF-16")
+}
+
+func Test_ContentType_AcceptingEmptyCharset_ShouldAcceptAllCharsets(t *testing.T) {
+	handler := Chain(NewContentTypeChecker().SetAcceptedContents("application/json").SetAcceptedCharset().Check).Then(nil)
+	acceptContentType(t, handler, "application/json; charset=UTF-8")
+	acceptContentType(t, handler, "application/json; charset=UTF-16")
+	acceptContentType(t, handler, "application/json; charset=any")
+}
+
+func acceptContentType(t *testing.T, handler http.Handler, contentType string) {
+	if processRequestWithContent(t, handler, contentType).Code != http.StatusOK {
+		t.Errorf("ContentType failure: sould have accepted %s", contentType)
+	}
+}
+
+func rejectContentType(t *testing.T, handler http.Handler, contentType string) {
+	if processRequestWithContent(t, handler, contentType).Code != http.StatusUnsupportedMediaType {
+		t.Errorf("ContentType failure: sould have rejected %s", contentType)
+	}
+}
+
+func processRequestWithContent(t *testing.T, handler http.Handler, contentType string) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header.Set("Content-Type", contentType)
+	handler.ServeHTTP(recorder, request)
+	return recorder
+}
